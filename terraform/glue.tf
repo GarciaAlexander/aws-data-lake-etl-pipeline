2ce410c3323cc9a3d551cb6af5d7acc793c1ae3b
AlexPR704 <alexg97343@gmail.com>
2026-07-27 20:56:14 -0400

    Add Glue crawler and Glue database

---
+++ terraform/glue.tf
@@ -0,0 +1,23 @@
+############################################
+# Glue Database
+############################################
+
+resource "aws_glue_catalog_database" "raw_db" {
+  name = "${var.project_name}-raw-db"
+}
+
+############################################
+# Glue Crawler
+############################################
+
+resource "aws_glue_crawler" "raw_crawler" {
+  name         = "${var.project_name}-raw-crawler"
+  role         = aws_iam_role.glue_role.arn
+  database_name = aws_glue_catalog_database.raw_db.name
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.raw.bucket}/"
+  }
+
+  classifiers = []
+}
